6d11416824a9842cab660c2648edf949de920896
dikdudu <dikdudu@hotmail.com>
2021-06-23 15:15:20 -0300

    configurando google sign in

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -53,9 +55,7 @@ class _LoginPageState extends State<LoginPage> {
                     padding:
                         const EdgeInsets.only(right: 40, left: 40, top: 30),
                     child: SocialLoginButton(
-                      onTap: () {
-                        print('clicou');
-                      },
+                      onTap: _controller.googleSignIn,
                     ),
                   ),
                 ],
