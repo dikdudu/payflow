b2bccfafbf746fd60514fc4527ea97203d1a14fc
dikdudu <dikdudu@hotmail.com>
2021-06-24 21:42:48 -0300

    criando label_button

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   const BarcodeScannerPage({Key? key}) : super(key: key);
@@ -42,6 +43,10 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: LabelButton(
+        label: "Inserir código do boleto",
+        onPressed: () {},
       ),
     );
   }
