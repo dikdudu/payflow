16c96142eb531dad93c685e4ff5d1f67772b440c
unknown <dikdudu@hotmail.com>
2021-06-27 11:27:34 -0300

    adcitionando bottom_sheet_widget

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
@@ -13,47 +14,61 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return RotatedBox(
-      quarterTurns: 1,
-      child:SafeArea(
+    return BottomSheetWidget(
+      title: "Não foi possivel indentificar o código de barras",
+      subtitle: "Tente escanear novamente ou digite o código do seu boleto",
+      primaryLabel: "Escanear Novamente",
+      primaryOnPressed: () {},
+      secudaryLabel: "Digitar codigo",
+      secudaryOnPressed: () {},
+    );
+    return SafeArea(
       top: true,
       bottom: true,
-      right: true,
       left: true,
-          child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.black,
-          title: Text(
-            "Escaneia o código de barras do boleto",
-            style: AppTextStyles.buttonBackground,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escaneia o código de barras do boleto",
+              style: AppTextStyles.buttonBackground,
+            ),
+            leading: BackButton(
+              color: AppColors.background,
+            ),
           ),
-          leading: BackButton(
-            color: AppColors.background,
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.transparent,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: SetLabelButton(
+            primaryLabel: "Inserir código do boleto",
+            primaryOnPressed: () {},
+            secudaryLabel: "Adicionar da galeria",
+            secudaryOnPressed: () {},
           ),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.black,
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.transparent,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: SetLabelButton(primaryLabel: "Inserir código do boleto",primaryOnPressed: (){},secudaryLabel: "Adicionar da galeria",secudaryOnPressed: (){},),
       ),
-    ),);
+    );
   }
 }
--- lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
@@ -7,29 +8,38 @@ class SetLabelButton extends StatelessWidget {
   final VoidCallback primaryOnPressed;
   final String secudaryLabel;
   final VoidCallback secudaryOnPressed;
-  const SetLabelButton({ Key? key,required this.primaryLabel,required this.primaryOnPressed,required this.secudaryLabel,required this.secudaryOnPressed }) : super(key: key);
+  final bool enablePrimaryColor;
+  const SetLabelButton(
+      {Key? key,
+      required this.primaryLabel,
+      required this.primaryOnPressed,
+      required this.secudaryLabel,
+      required this.secudaryOnPressed,
+      this.enablePrimaryColor = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 56,
-        child: Row(
-          children: [
-            Expanded(
-              child: LabelButton(
-                label: primaryLabel,
-                onPressed: primaryOnPressed,
-              ),
+      height: 56,
+      child: Row(
+        children: [
+          Expanded(
+            child: LabelButton(
+              label: primaryLabel,
+              onPressed: primaryOnPressed,
+              style: enablePrimaryColor ? AppTextStyles.buttonPrimary : null,
             ),
-            DividerVerticalWidget(),
-            Expanded(
-              child: LabelButton(
-                label: secudaryLabel,
-                onPressed: secudaryOnPressed,
-              ),
+          ),
+          DividerVerticalWidget(),
+          Expanded(
+            child: LabelButton(
+              label: secudaryLabel,
+              onPressed: secudaryOnPressed,
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
