1fe1d0d4b9823b2bacf98b715bc14daf2fd042d2
unknown <dikdudu@hotmail.com>
2021-06-26 22:47:01 -0300

    adicionando set_label_buttons

---
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
+import 'package:payflow/shared/widgets/label_button/label_button.dart';
+
+class SetLabelButton extends StatelessWidget {
+  final String primaryLabel;
+  final VoidCallback primaryOnPressed;
+  final String secudaryLabel;
+  final VoidCallback secudaryOnPressed;
+  const SetLabelButton({ Key? key,required this.primaryLabel,required this.primaryOnPressed,required this.secudaryLabel,required this.secudaryOnPressed }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 56,
+        child: Row(
+          children: [
+            Expanded(
+              child: LabelButton(
+                label: primaryLabel,
+                onPressed: primaryOnPressed,
+              ),
+            ),
+            DividerVerticalWidget(),
+            Expanded(
+              child: LabelButton(
+                label: secudaryLabel,
+                onPressed: secudaryOnPressed,
+              ),
+            ),
+          ],
+        ),
+      );
+  }
+}
