1e20f8eee98949a57df412a28e633e2dc95b3391
unknown <dikdudu@hotmail.com>
2021-06-27 11:27:08 -0300

    adicionando bottom_sheet_widget

---
+++ lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
+
+class BottomSheetWidget extends StatelessWidget {
+  final String primaryLabel;
+  final VoidCallback primaryOnPressed;
+  final String secudaryLabel;
+  final VoidCallback secudaryOnPressed;
+  final String title;
+  final String subtitle;
+  const BottomSheetWidget(
+      {Key? key,
+      required this.primaryLabel,
+      required this.primaryOnPressed,
+      required this.secudaryLabel,
+      required this.secudaryOnPressed,
+      required this.title,
+      required this.subtitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: AppColors.shape,
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.black.withOpacity(0.6),
+            )),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(40),
+                  child: Text.rich(
+                    TextSpan(
+                      text: title,
+                      style: AppTextStyles.buttonBoldHeading,
+                      children: [
+                        TextSpan(
+                            text: "\n$subtitle",
+                            style: AppTextStyles.buttonHeading),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  height: 1,
+                  color: AppColors.stroke,
+                ),
+                SetLabelButton(
+                  enablePrimaryColor: true,
+                  primaryLabel: primaryLabel,
+                  primaryOnPressed: primaryOnPressed,
+                  secudaryLabel: secudaryLabel,
+                  secudaryOnPressed: secudaryOnPressed,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
